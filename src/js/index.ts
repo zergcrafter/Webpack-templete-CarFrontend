cdcf5fb8d1ad7130a263a6b7e8c394033a0c0292
Vitus Nørgaard Rossing Schuster <vitu0016@edu.easj.dk>
2019-03-27 12:26:43 +0100

    tilføjer basis axios command

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,24 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios,{
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+import { ICar } from "./Icar";
+
+let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+
+function ShowAllCars():void{
+
+    axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
+    .then(function(response:AxiosResponse<ICar[]>) : void
+    {
+        console.log(response);
+    })
+    .catch(
+        function(error:AxiosError) : void{
+            console.log(error);
+        }
+    )
+    console.log("Er i slutningen af ShowAllCars Funtion");
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+ShowAllCars();
