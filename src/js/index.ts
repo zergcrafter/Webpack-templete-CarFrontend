9ff7d0178346e85e0287bb8f09a0e5d36414228b
Vitus Nørgaard Rossing Schuster <vitu0016@edu.easj.dk>
2019-03-27 13:41:51 +0100

    Get and Post

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,8 +3,36 @@ import axios,{
     AxiosError
 } from "../../node_modules/axios/index"
 import { ICar } from "./Icar";
+import Axios from "../../node_modules/axios/index";
 
 let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+let GetAllCarsButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("getAllButton");
+let AddCarButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("addButton");
+
+GetAllCarsButton.addEventListener('click', ShowAllCars);
+AddCarButton.addEventListener('click', addCar);
+
+function addCar():void{
+    let addModelelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addModel");
+    let addVendorelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addVendor");
+    let addPriceelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addPrice");
+
+    let myModel : string = addModelelement.value;
+    let myVendor : string = addVendorelement.value;
+    let myPrice : number = +addPriceelement.value;
+
+    axios.post<ICar>("https://webapicar20190326034339.azurewebsites.net/api/cars",
+    {model:myModel, vendor:myVendor, price:myPrice})
+    .then(function(response:AxiosResponse) : void
+    {
+        console.log("Status er: " + response.status)
+    })
+    .catch(
+        function (error:AxiosError) : void{
+        console.log(error);
+        }
+    )
+}
 
 function ShowAllCars():void{
 
@@ -12,6 +40,15 @@ function ShowAllCars():void{
     .then(function(response:AxiosResponse<ICar[]>) : void
     {
         console.log(response);
+
+        let result: string = "<ol>"
+
+        response.data.forEach((car: ICar) => {
+            result += "<li>" + car.model + " " + car.vendor + " " + car.price + "</li>"
+        });
+        result += "</ol>"
+
+        ContentElement.innerHTML = result;
     })
     .catch(
         function(error:AxiosError) : void{
@@ -21,4 +58,4 @@ function ShowAllCars():void{
     console.log("Er i slutningen af ShowAllCars Funtion");
 }
 
-ShowAllCars();
+// ShowAllCars();
